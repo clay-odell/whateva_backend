c59889512a3a6903872684999c6f91fb304aca69
Clay O'Dell <clayodell.swe@gmail.com>
2025-05-21 18:15:41 -0500

    updated db.js

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
 
-const pool = new Pool ({
-    user: 'postgres',
-    host: "localhost",
-    database: "whateva_web",
-    password: "Iggy1302",
-    port: 5432
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: parseInt(process.env.PGPORT, 10)
 });
 
 module.exports = pool;
